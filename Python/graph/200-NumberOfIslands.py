e7f38aad6b1e1a76dcd2501ff4a6ff4b575b2c69
Sathyajit Loganathan <loganathansathyajit@gmail.com>
2026-02-04 23:56:33 -0600

    Remove function

--- Python/graph/200-NumberOfIslands.py
+++ Python/graph/200-NumberOfIslands.py
@@ -34,35 +34,3 @@ class Solution():
                     count += 1
         return count
     
-     def numIslands2(self, grid: List[List[str]]) -> int:
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        count = 0
-
-        def bfs(r: int, c: int) -> int:
-            q = deque()
-            visited.add((r,c))
-            q.append((r,c))
-
-            directions = [ [0, 1], [0, -1], [-1, 0], [1, 0] ]
-
-            while q:
-                row, col = q.popleft()            
-                for dr, dc in directions:
-                    r,c = row+dr, col+dc;
-
-                    if (r in range(rows) and c in range(cols) and grid[r][c]=='1' and (r,c) not in visited):
-                        q.append((r,c))
-                        visited.add((r,c))
-                
-            return 1
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1' and (r,c) not in visited:
-                    bfs(r,c)
-                    count += 1
-        return count
-    
