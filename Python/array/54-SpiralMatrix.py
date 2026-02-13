fa4967184a6031d724599646f3c27fa90963c09e
Sathyajit Loganathan <loganathansathyajit@gmail.com>
2026-02-12 23:21:16 -0600

    Add base structure for SPiral Matrix

--- Python/array/54-SpiralMatrix.py
+++ Python/array/54-SpiralMatrix.py
@@ -1,13 +1,15 @@
-from typing import List
-
-
 class Solution:
-    def spiralOrder(self, nums: List[List[int]]):
-        bottom = len(nums)
-        right = len(nums[0])
-        top = 0
-        left = 0
+  def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+    if not matrix:
+      return []
 
-        while n < max_n and m < max_m:
+    m = len(matrix)
+    n = len(matrix[0])
+    ans = []
+    r1 = 0
+    c1 = 0
+    r2 = m - 1
+    c2 = n - 1
 
-            print(m, n)
+
+    return ans
